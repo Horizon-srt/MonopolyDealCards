404dda3812d78ef719b5bfca77f016b6d21bdbcc
liuhuiyang99 <2091458493@qq.com>
2023-06-12 15:36:18 +0800

    some interface in view and some card implementation

--- src/Core/ITerminalView.java
+++ src/Core/ITerminalView.java
@@ -14,7 +14,7 @@ public interface ITerminalView {
     // 得到玩家想要加入到对应财产堆的序号
     public int getIndex();
 
-    // 返回 slyCard 或 ForceDeal 或 dealbreaker 的目标玩家
+    // 返回 slyCard 或 ForceDeal 或 dealbreaker 或 debt collector 的目标玩家
     public Player getTargetplayer();
 
     // 返回 slyCard 或 ForceDeal 的目标财产
@@ -26,4 +26,6 @@ public interface ITerminalView {
     // DealBreaker 获取目标财产堆并删除
     public LinkedList<IPropertyCard> getProperty();
 
+    // justsayno 询问目标玩家是否使用 justsayno 并返回 true 或false
+    public boolean askJustSayNo(Player q);
 }
--- src/Modules/Cards/impl/DebtCollectorCard.java
+++ src/Modules/Cards/impl/DebtCollectorCard.java
@@ -1,7 +1,28 @@
 package Modules.Cards.impl;
 
+import Core.GameController;
+import Core.TerminalView;
+import Modules.Player.impl.Player;
+import utils.Type;
+
 public class DebtCollectorCard extends ActionCard{
     public DebtCollectorCard(String name, int value) {
         super(name, value);
+    }
+
+    @Override
+    public void use(GameController g, Player p) {
+        TerminalView tv = g.tv;
+        Player q = tv.getTargetplayer();
+        for (Card card : tv.rent(p, q, 5)) {
+            if (card.getType() == Type.MONEY || card.getType() == Type.ACTION) {
+                p.getBp().setCard(card);
+                q.getBp().removeCard(card);
+            } else {
+                p.getPp().setCard(card);
+                q.getPp().removeCard(card);
+            }
+        }
+
     }
 }
