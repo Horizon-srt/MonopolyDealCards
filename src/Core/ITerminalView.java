b3b239849d128a10e31385ec198782110181fa85
Horizon-srt <horizon_srt@outlook.com>
2023-06-12 18:05:43 +0800

    feat(view interface): view interface

--- src/Core/ITerminalView.java
+++ src/Core/ITerminalView.java
@@ -3,6 +3,7 @@ package Core;
 import Modules.Cards.iface.IPropertyCard;
 import Modules.Cards.impl.Card;
 import Modules.Cards.impl.PropertyCard;
+import Modules.Piles.impl.PropertyPile;
 import Modules.Player.impl.Player;
 
 import java.util.LinkedList;
@@ -15,17 +16,17 @@ public interface ITerminalView {
     public int getIndex();
 
     // 返回 slyCard 或 ForceDeal 或 dealbreaker 或 debt collector 的目标玩家
-    public Player getTargetplayer();
+    public Player getTargetplayer(Player[] player);
 
     // 返回 slyCard 或 ForceDeal 的目标财产
-    public PropertyCard slyForceDealTargetProperty();
+    public PropertyCard slyForceDealTargetProperty(Player player);
 
     // 返回 ForceDeal 玩家自己想要交换的财产
-    public PropertyCard forceDealOwnProperty();
+    public PropertyCard forceDealOwnProperty(Player player);
 
     // DealBreaker 获取目标财产堆并删除
-    public LinkedList<IPropertyCard> getProperty();
+    public LinkedList<IPropertyCard> getProperty(PropertyPile pPile);
 
-    // justsayno 询问目标玩家是否使用 justsayno 并返回 true 或false
-    public boolean askJustSayNo(Player q);
+    // justsayno 询问目标玩家是否使用 justsayno 并返回 卡片序号 或 -1
+    public int askJustSayNo(Player q, String cardName);
 }
